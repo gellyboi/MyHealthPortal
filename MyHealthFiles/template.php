e6b7d5f1d9b3e7fd5bf30be6092ba271c01ee401
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-05-01 13:26:00 -0700

    template to use for each page

---
+++ MyHealthFiles/template.php
@@ -0,0 +1,50 @@
+<?php
+    include('server.php');
+?>
+<?php
+    if (!isset($_SESSION['name'])) {
+        $_SESSION['msg'] = "Login required";
+        header('location: login.php');
+    }
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Health Portal</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<!-- HEADER PANEL -->
+	<div id="header-panel">
+		<h1 class="center-text">My Health Portal</h1>
+		
+	</div>
+	
+	<!-- SIDE PANEL -->
+	<div id="side-panel">
+		<h2 class="top-text">Welcome, <?php echo $_SESSION['name']; ?>!</h2>
+		<p class="itemize"><a href="index.php">Home</a></p>
+		<p class="itemize"><a href="appointments.php">Create Appointments</a></p>
+		<p class="itemize"><a href="index.php">Purchase Pharmaceuticals</a></p>
+		<p class="itemize"><a href="insurancePlans.php">Review Your Insurance Plans</a></p>
+		<p class="itemize"><a href="index.php">See Your Billing Statements</a></p>
+		
+		<form method="post" action="index.php">
+			<button type="submit" name="logout">Sign Out</button>
+		</form>
+	</div>
+	
+	<!-- BODY PANEL -->
+	<div id="center-panel">
+		<h2 class="top-text">Body Header</h2>
+		<p>stuff goes here</p>
+	</div>
+	
+	<!-- FOOTER PANEL -->
+		<div id="footer-panel">
+			<h3>Created by: Ryan Buckel, Christian Whitfield</h3>
+		</div>
+	</body>
+</html>
