d0d640d10684c93d8ef074bcb869fbe315ea7191
gellyboi <78063792+gellyboi@users.noreply.github.com>
2021-05-04 20:49:42 -0700

    FIXED!!!

--- MyHealthFiles/index.php
+++ MyHealthFiles/index.php
@@ -4,7 +4,7 @@
 ?>
 <?php
     //session_start();
-    if (!isset($_SESSION['pid']) || !isset($_SESSION['did'])){
+    if (!isset($_SESSION['name'])){
         $_SESSION['msg'] = "Login required";
         header('location: login.php');
     }
